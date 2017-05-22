5bcd459d78aa933e7ae0fd934b835dbb38dd08fc
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-05-22 20:59:20 +0100

    some stuff

--- concurrent_programs/src/main.ts
+++ concurrent_programs/src/main.ts
@@ -131,3 +131,20 @@ subject.subscribe(
     function complete() { console.log(`Complete`)}
 )
 */
+
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var SPEED = 40;
+var STAR_NUMBER = 250;
+var StarStream = Rx.Observable.range(1, STAR_NUMBER)
+    .map(() => {
+        return {
+            x: Math.floor(Math.random() * canvas.width),
+            y: Math.floor(Math.random() * canvas.height),
+            size: Math.random() * 3 + 1
+        };
+    })
