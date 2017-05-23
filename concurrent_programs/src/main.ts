d663fc57341d5c55babfbedeacada43dbba8fa29
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-05-23 15:03:24 +0100

    Created the video game

--- concurrent_programs/src/main.ts
+++ concurrent_programs/src/main.ts
@@ -132,15 +132,43 @@ subject.subscribe(
 )
 */
 
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext("2d");
+function paintStars(stars){
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#ffffff';
+    stars.forEach( (star) => {
+        ctx.fillRect(star.x, star.y, star.size, star.size);
+    })
+}
+
+function drawTriangle(x, y, width, color, direction) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x - width, y);
+    ctx.lineTo(x, direction === 'up' ? y - width : y + width);
+    ctx.lineTo(x + width, y);
+    ctx.lineTo(x - width, y);
+    ctx.fill();
+}
+
+function paintSpaceShip(x, y) {
+    drawTriangle(x, y, 20, '#ff0000', 'up');
+}
+
+function renderScene(actors) {
+    paintStars(actors.stars);
+    paintSpaceShip(actors.spaceship.x, actors.spaceship.y);
+}
+
+var canvas:HTMLCanvasElement = document.createElement('canvas');
+var ctx:CanvasRenderingContext2D = canvas.getContext("2d");
 document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var SPEED = 40;
-var STAR_NUMBER = 250;
-var StarStream = Rx.Observable.range(1, STAR_NUMBER)
+var SPEED:number = 40;
+var STAR_NUMBER:number = 250;
+var StarStream: Observable<any> = Rx.Observable.range(1, STAR_NUMBER)
     .map(() => {
         return {
             x: Math.floor(Math.random() * canvas.width),
@@ -148,3 +176,51 @@ var StarStream = Rx.Observable.range(1, STAR_NUMBER)
             size: Math.random() * 3 + 1
         };
     })
+    .toArray()
+    .flatMap( (starArray) => {
+        return Rx.Observable.interval(SPEED).map( () => {
+            starArray.forEach( (star) => {
+                if (star.y >= canvas.height) {
+                    star.y = 0 // reset star to top of screen
+                }
+                star.y += 3;
+            });
+            return starArray;
+        });
+    });
+
+
+var HERO_Y = canvas.height - 30;
+var mouseMove = Rx.Observable.fromEvent<MouseEvent>(canvas, 'mousemove');
+var SpaceShip = mouseMove
+    .map( (event) => {
+        return {
+            x: event.clientX,
+            y: HERO_Y
+        };
+    })
+    .startWith({
+        x: canvas.width / 2,
+        y: HERO_Y
+    });
+
+var ENEMY_FREQ = 1500;
+var Enemies:Observable<any> = Rx.Observable.interval(ENEMY_FREQ)
+    .scan( (enemyArray: any[]) => {
+        var enemy = {
+            x: Math.floor(Math.random()) * canvas.width,
+            y: -30
+        };
+
+        enemyArray.push(enemy);
+        return enemyArray;
+    }, []);
+
+var Game = Rx.Observable
+    .combineLatest(
+        StarStream, SpaceShip,
+        (stars, spaceship) => {
+            return { stars: stars, spaceship: spaceship };
+    });
+
+Game.subscribe(renderScene)
